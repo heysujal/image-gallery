4505923495241e21bcac7c3da6054f2429e63315
Sujal Gupta <sujalgupta6100@gmail.com>
2026-03-26 00:36:04 +0530

    use react-window for virual list
    Signed-off-by: Sujal Gupta <sujalgupta6100@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 
-import './App.css'
+import {List} from "react-window";
 
 function App() {
-  const n = 1000
-  const images = Array(n).fill(0)
-  console.log(images);
-  
+  const n = 1000;
+  const images = Array(n).fill(0).map((_, i) => {
+    return `https://picsum.photos/300/200?random=${i}`
+  })
+
+  const Row = ({ index, style , images}) => (
+    <div style={style}>  {/* style is REQUIRED — controls positioning */}
+      <img
+        loading="lazy"
+        width={100}
+        height={100}
+        src={images[index]}
+        alt={`img-${index}`}
+      />
+    </div>
+  );
 
   return (
-    images?.map((x, i) => {
-       return <img loading='lazy' width={100} height={100} key={i} src={`https://picsum.photos/300/200?random=${i}`} alt={`img-${i}`}/>
-    })
-  )
+    <List
+      rowComponent={Row}
+      rowCount={n}       // total number of items
+      rowHeight={100}      // height of each row in px
+      rowProps={{images: images}}
+      style={{height: '100px'}}
+    />
+
+  );
 }
 
 export default App
